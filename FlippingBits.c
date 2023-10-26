d79759dec5c53496ce4aa94db3db08c80751703f
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-10-26 22:09:53 +0530

    solution added

--- FlippingBits.c
+++ FlippingBits.c
@@ -1,18 +1,3 @@
-// You will be given a list of 32 bit unsigned integers. Flip all the bits ( 1->0 and 0->1) and return the result as an unsigned integer.
-
-// Function Description
-
-// Complete the flippingBits function in the editor below.
-
-// flippingBits has the following parameter(s):
-
-// int n: an integer
-// Returns
-
-// int: the unsigned decimal integer result
-
-// Link to the Problem:https://www.hackerrank.com/challenges/flipping-bits/problem
-
 long flippingBits(long n) {
 return 4294967295-n;
 }
--- MigratoryBirds.js
+++ MigratoryBirds.js
@@ -1,19 +1,3 @@
-//Given an array of bird sightings where every element represents a bird type id, determine the id of the most frequently sighted type. 
-//If more than 1 type has been spotted that maximum amount, return the smallest of their ids.
-
-// Function Description
-
-// Complete the migratoryBirds function in the editor below.
-
-// migratoryBirds has the following parameter(s):
-
-// int arr[n]: the types of birds sighted
-// Returns
-
-// int: the lowest type id of the most frequently sighted birds
-
-//  link to the problem:https://www.hackerrank.com/challenges/migratory-birds/problem
-
 function migratoryBirds(arr) {
   let sorted = arr.sort();
   let ans = sorted[0];
