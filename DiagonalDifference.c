1f9177567182c1f4ce22fd22e411024d3a940622
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-10-26 22:09:40 +0530

    solution added

--- DiagonalDifference.c
+++ DiagonalDifference.c
@@ -1,16 +1,3 @@
-// Given a square matrix, calculate the absolute difference between the sums of its diagonals.
-
-// Function description
-
-// diagonalDifference takes the following parameter:
-
-// int arr[n][m]: an array of integers
-// Return
-
-// int: the absolute diagonal difference
-
- // link to the problem:https://www.hackerrank.com/challenges/diagonal-difference/problem
-
 int diagonalDifference(int arr_rows, int arr_columns, int** arr) {
  int pdsum=0,sdsum=0,diff=0;
  for(int i=0;i<arr_columns;i++){
